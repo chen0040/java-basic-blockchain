597378747db0b4ff9b079ab8ce3a0a5b8790feec
xschen <xs0040@gmail.com>
2018-03-11 22:21:12 +0800

    implement node registration and chain validation

--- src/main/java/com/github/chen0040/blockchain/BlockChain.java
+++ src/main/java/com/github/chen0040/blockchain/BlockChain.java
@@ -6,16 +6,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Getter
 @Setter
 public class BlockChain {
     private List<Block> chain = new ArrayList<>();
     private List<Transaction> currentTransactions = new ArrayList<>();
+    private Set<String> nodes = new HashSet<>();
 
     public Block newBlock(long proof) {
         return newBlock(proof, null);
@@ -59,9 +57,51 @@ public class BlockChain {
 
     private String hash(Block block) {
         String json = JSON.toJSONString(block);
-        String sha256hex = Hashing.sha256()
-                .hashString(json, StandardCharsets.UTF_8)
-                .toString();
-        return sha256hex;
+       return hash(json);
     }
+
+    private String hash(String text) {
+        return Hashing.sha256()
+                .hashString(text, StandardCharsets.UTF_8)
+                .toString();
+    }
+
+    public boolean validateProof(long lastProof, long proof) {
+        String text = lastProof + "" + proof;
+        String hashed = hash(text);
+        return hashed.startsWith("0000");
+    }
+
+    public long proofOfWork(long lastProof) {
+        long proof = 0L;
+        while(!validateProof(lastProof,proof)) {
+            proof++;
+        }
+        return proof;
+    }
+
+    public void registerNode(String url) {
+        nodes.add(url);
+    }
+
+    public boolean validateChain(List<Block> chain) {
+        Block lastBlock = chain.get(0);
+        int currentIndex = 1;
+        while(currentIndex < chain.size()) {
+            Block block = chain.get(currentIndex);
+            if(!block.getPrevHash().equals(hash(lastBlock))) {
+                return false;
+            }
+            lastBlock = block;
+            currentIndex++;
+        }
+
+        return true;
+    }
+
+
+
+
+
+
 }
